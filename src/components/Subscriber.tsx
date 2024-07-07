99e6a0930c654a5ffb38d2263d4e85df512ab4a2
osvaldocariege06 <osvaldocariege06@gmail.com>
2024-07-07 18:28:51 +0100

    Ol

--- src/components/Subscriber.tsx
+++ src/components/Subscriber.tsx
@@ -4,19 +4,19 @@ import { ArrowRightIcon } from 'lucide-react'
 
 export default function Subscriber() {
   return (
-    <div className="bg-image-subscriber  object-cover bg-center  p-40 w-full h-[955px]">
-      <div className="max-w-[1113px] w-full mx-auto">
-        <div className="w-[508px] h-[396px] bg-[#57AC49] rounded p-[42px]">
+    <div className="h-[955px] w-full bg-image-subscriber bg-center object-cover p-40">
+      <div className="mx-auto w-full max-w-[1113px]">
+        <div className="h-[396px] w-[508px] rounded bg-[#57AC49] p-[42px]">
           <span className="text-2xl font-medium">Subscriber</span>
-          <h2 className="text-white text-[32px] font-mochiyPop max-w-[402px]">
+          <h2 className="max-w-[402px] font-mochiyPop text-[32px] text-white">
             Sign up as our partner and start selling
           </h2>
-          <p className="text-zinc-100 mt-4">
+          <p className="mt-4 text-zinc-100">
             Use our built-in analytics dashboard to pull valuable insights and
             monitor the value of your Krypto portfolio over time.{' '}
           </p>
 
-          <div className="w-[215px] mt-7">
+          <div className="mt-7 w-[215px]">
             <Button color="white" text="become a partner">
               <ArrowRightIcon color="#57AC49" />
             </Button>
