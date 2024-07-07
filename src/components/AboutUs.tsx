1a3b887c5327081b32c928e9ee51b71e706b2b86
osvaldocariege06 <osvaldocariege06@gmail.com>
2024-07-07 17:53:22 +0100

    Resolving abou us - button component - citation

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -3,23 +3,23 @@ import { ArrowRightIcon } from 'lucide-react'
 
 export default function AboutUs() {
   return (
-    <div className="flex items-end justify-between py-28 max-w-[1113px] w-full mx-auto">
+    <div className="mx-auto flex w-full max-w-[1113px] items-end justify-between py-28">
       <div>
-        <h2 className="text-[#57AC49] font-bold text-xl uppercase">
+        <h2 className="text-xl font-bold uppercase text-[#57AC49]">
           01. AboutUs
         </h2>
-        <p className="font-mochiyPop text-[40px] max-w-[494px] w-full">
+        <p className="w-full max-w-[494px] font-mochiyPop text-[40px]">
           helping people to live better than before.
         </p>
       </div>
 
       <div className="">
-        <p className="max-w-[550px] w-full text-zinc-400 text-base mb-4">
+        <p className="mb-4 w-full max-w-[550px] text-base text-zinc-400">
           Use our built-in analytics dashboard to pull valuable insights and
           monitor the value of your Krypto portfolio over time. Use our built-in
           of your Krypto portfolio over time.
         </p>
-        <span className="text-[#57AC49] flex gap-1 active:scale-95 transition-all cursor-pointer">
+        <span className="flex cursor-pointer gap-1 text-[#57AC49] transition-all active:scale-95">
           Learn more
           <ArrowRightIcon color="#00cc00" />
         </span>
--- src/components/Citation.tsx
+++ src/components/Citation.tsx
@@ -5,15 +5,15 @@ import React from 'react'
 
 export default function Citation() {
   return (
-    <div className="py-28 max-w-[1113px] w-full mx-auto">
-      <h2 className="text-[#57AC49] font-bold text-xl uppercase">
+    <div className="mx-auto w-full max-w-[1113px] py-28">
+      <h2 className="text-xl font-bold uppercase text-[#57AC49">
         03. CITATION
       </h2>
-      <h3 className="text-[40px] font-mochiyPop max-w-[496px]">
+      <h3 className=" max-w-[496px] font-mochiyPop text-[40px]">
         Discover the benefits of working with us
       </h3>
 
-      <div className="flex items-center justify-between w-full">
+      <div className="w-full items-center justify-between">
         <div className="flex flex-col gap-4">
           <div className="flex items-start gap-2">
             <Image
@@ -23,16 +23,16 @@ export default function Citation() {
               height={24}
               className="pb-4"
             />
-            <p className="font-mochiyPop text-xl max-w-[386px]">
+            <p className="max-w-[386px] font-mochiyPop text-xl">
               We offer you quality work, safety and comfort in producing your
               products.
             </p>
           </div>
-          <p className="text-base font-normal text-zinc-400 max-w-[350px] ml-8">
+          <p className="ml-8 max-w-[350px] text-base font-normal text-zinc-400">
             Use our built-in analytics dashboard to pull valuable insights and
             monitor the value of your Krypto portfolio over time.
           </p>
-          <span className="text-[#57AC49] flex gap-1 active:scale-95 transition-all cursor-pointer ml-8">
+          <span className="ml-8 flex cursor-pointer gap-1 text-[#57AC49] transition-all active:scale-95">
             Learn more
             <ArrowRightIcon color="#00cc00" />
           </span>
@@ -42,16 +42,16 @@ export default function Citation() {
           <Image
             src={citation01}
             alt="01.Citation image"
-            className="rounded w-[357px] h-[455px] shadow"
+            className="h-[455px] w-[357px] rounded"
           />
           <div className="mt-8">
-            <p className="text-[13px] max-w-[254px] w-full font-normal text-zinc-400">
+            <p className="w-full max-w-[254px] text-[13px] text-zinc-400">
               Building the future through technology with agriculture.
             </p>
             <Image
               src={citation02}
               alt="02.Citation image"
-              className="mt-[72px] w-[265px] h-[337px]"
+              className="mt-[72px] h-[337px] w-[265]"
             />
           </div>
         </div>
